90802e764a74b6d75fc56e4df95461b32309c352
Eunjae218 <eunjae.yu5981@gmail.com>
2024-09-11 18:37:15 +0700

    tried a2

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
+a1
 let message = document.getElementById("msg");
 let result = document.getElementById("result");
 let points = [34, 94, 54, 101, 5, 41]
 
 function ascending() {
-    message.innerHTML = "Array in ascending order"
-    result.innerHTML = points.sort(function (a, b) { return a - b })
+   message.innerHTML = "Array in ascending order"
+   result.innerHTML = points.sort(function (a, b) { return a - b })
 }
 
 function descending() {
-    message.innerHTML = "Array in descending order"
-    result.innerHTML = points.reverse(function (a, b) { return a + b })
+   message.innerHTML = "Array in descending order"
+   result.innerHTML = points.reverse(function (a, b) { return a + b })
 }
+
+//a2
+let resutl = document.getElementById("resutl")
+let multiplynum = [24, 76, 63, 37]
+
+function multen(num){
+    return num*10
+}
+
+//resutl.innerHTML = multiplynum.map(multen)
+console.log(multiplynum.map(multen))
