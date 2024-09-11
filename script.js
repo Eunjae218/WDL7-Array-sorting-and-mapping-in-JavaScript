436041d2b065b1e7ddb79711f486cdd4e619df50
Eunjae218 <eunjae.yu5981@gmail.com>
2024-09-11 18:24:27 +0700

    add ascending + descending order function for a1

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
 let message = document.getElementById("msg");
 let result = document.getElementById("result");
+let points = [34, 94, 54, 101, 5, 41]
+
+function ascending() {
+    message.innerHTML = "Array in ascending order"
+    result.innerHTML = points.sort(function (a, b) { return a - b })
+}
+
+function descending() {
+    message.innerHTML = "Array in descending order"
+    result.innerHTML = points.reverse(function (a, b) { return a + b })
+}
