a7b287e53aa53fd6ffe2b3d0dc915ebbcc4e49d5
Eunjae218 <eunjae.yu5981@gmail.com>
2024-09-13 17:56:24 +0700

    identified the issue and resolved it (in line 3 getelementbyiid)

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
-a1
-let message = document.getElementById("msg");
 let result = document.getElementById("result");
-let points = [34, 94, 54, 101, 5, 41]
+let points = [34, 94, 54, 101, 5, 41];
+let message = document.getElementById("msg");
 
 function ascending() {
    message.innerHTML = "Array in ascending order"
@@ -12,14 +11,3 @@ function descending() {
    message.innerHTML = "Array in descending order"
    result.innerHTML = points.reverse(function (a, b) { return a + b })
 }
-
-//a2
-let resutl = document.getElementById("resutl")
-let multiplynum = [24, 76, 63, 37]
-
-function multen(num){
-    return num*10
-}
-
-//resutl.innerHTML = multiplynum.map(multen)
-console.log(multiplynum.map(multen))
